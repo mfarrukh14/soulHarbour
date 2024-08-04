b466185f5cc350fe8936ea70c67833cfa0fba1f6
mfarrukh14 <87035938+mfarrukh14@users.noreply.github.com>
2024-08-04 18:05:54 +0500

    Discord bot ready

---
+++ index.js
@@ -0,0 +1,73 @@
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+const { OpenAI, OpenAIApi } = require('openai');
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', () => {
+  console.log('The bot is online!');
+});
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY // This is also the default, can be omitted
+  });
+
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith('!')) return;
+
+  let conversationLog = [
+    { role: 'assistant', content: 'You are a metaspirit guru that guides people about metaspirits and replies them in a wise spirit tone i.e, like a spirit nerd and very wise tone, occasionally plays trivia with the people about the metaspirits and keep them engaged, you write concise and brief messages.' },
+  ];
+
+  try {
+    await message.channel.sendTyping();
+    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+    prevMessages.reverse();
+    
+    prevMessages.forEach((msg) => {
+      if (msg.content.startsWith('!')) return;
+      if (msg.author.id !== client.user.id && message.author.bot) return;
+      if (msg.author.id == client.user.id) {
+        conversationLog.push({
+          role: 'assistant',
+          content: msg.content,
+          name: msg.author.username
+            .replace(/\s+/g, '_')
+            .replace(/[^\w\s]/gi, ''),
+        });
+      }
+
+      if (msg.author.id == message.author.id) {
+        conversationLog.push({
+          role: 'user',
+          content: msg.content,
+          name: message.author.username
+            .replace(/\s+/g, '_')
+            .replace(/[^\w\s]/gi, ''),
+        });
+      }
+    });
+
+    const result = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: conversationLog,
+        // max_tokens: 256, // limit token usage
+      })
+      .catch((error) => {
+        console.log(`OPENAI ERR: ${error}`);
+      });
+    message.reply(result.choices[0].message);
+  } catch (error) {
+    console.log(`ERR: ${error}`);
+  }
+});
+
+client.login(process.env.TOKEN);
